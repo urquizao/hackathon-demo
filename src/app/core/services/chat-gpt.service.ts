aa8b3b6190fc920c520d94fc6698aca745571761
Omar <urquiza.omar@ic.edu>
2024-04-25 22:05:27 -0500

    changes to the UI... make everything interact :)

--- src/app/core/services/chat-gpt.service.ts
+++ src/app/core/services/chat-gpt.service.ts
@@ -1,36 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatGPTService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
-
-  }
-
-  async communeWithTheAIGods(message:string): Promise<any> {
+  async communeWithTheAIGods(message: string): Promise<any> {
     return lastValueFrom(
       this.http.post<any>(
-       'https://api.openai.com/v1/chat/completions',
-       {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-          {
-            "role": "user",
-            "content": message
-          }
-        ]
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${environment.SECRET_GPT_API}`
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {
+              role: 'user',
+              content: message,
+            },
+          ],
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${environment.SECRET_GPT_API}`,
+          },
         }
-      }
-      ),
+      )
     );
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ChatContainerComponent } from '../../components/chat-container/chat-container.component';
 import { UserListComponent } from '../../components/user-list/user-list.component';
@@ -7,23 +8,29 @@ import { ChatType } from '../../types/chat.types';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ChatContainerComponent, UserListComponent, CommonModule],
+  imports: [
+    ChatContainerComponent,
+    UserListComponent,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  messageToSend: string | undefined;
   chatType: ChatType = ChatType.USER;
   userName: string = this.chatType == ChatType.AI ? 'User' : 'AI';
   messages: any[] | undefined = [
-    {
-      Sender: 'You',
-      Message: 'Hello',
-      Timestamp: '04/26/2022',
-    },
-    {
-      Sender: 'AI (Chatbot)',
-      Message: 'Hello I am Chat AI',
-      Timestamp: '04/26/2022',
-    },
+    // {
+    //   Sender: 'You',
+    //   Message: 'Hello',
+    //   Timestamp: '04/26/2022',
+    // },
+    // {
+    //   Sender: 'AI (Chatbot)',
+    //   Message: 'Hello I am Chat AI',
+    //   Timestamp: '04/26/2022',
+    // },
   ];
 
   myUsers: any[] = [
@@ -43,9 +50,16 @@ export class HomeComponent {
     },
     {
       UserId: 3,
-      FirstName: 'Test',
-      LastName: 'Me',
-
+      FirstName: 'John',
+      LastName: 'Doe',
+      Department: 'Frozen',
+      Email: 'test.me@dotfoods.com',
+    },
+    {
+      UserId: 4,
+      FirstName: 'Jane',
+      LastName: 'Dough',
+      Department: 'Frozen',
       Email: 'test.me@dotfoods.com',
     },
   ];
@@ -55,11 +69,19 @@ export class HomeComponent {
     this.chatType = e;
   }
 
-  test(e: any) {
-    console.log('Hello World');
+  selectUser(e: any, user: any) {
+    this.selectedUser = user;
   }
 
   setUserName(e: any) {
-    this.userName = e.target.value;
+    this.userName = e.target;
+  }
+
+  send(event: any) {
+    this.messages?.push({
+      Sender: 'You',
+      Message: this.messageToSend,
+      Timestamp: '04/26/2022',
+    });
   }
 }
